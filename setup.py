f97ad3e0bd35c079ce1f4ba688b92138a48630f8
Maximilian <maximilian.muschalik@gmail.com>
2025-07-25 10:53:50 +0200

    adjusted setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ TREEPATH = "granite/tree_shap"
 
 setup(
     ext_modules=[Extension('granite.treeshap', sources=[f"{TREEPATH}/main.cpp"], 
+                           extra_compile_args=["-std=c++11"],
                            depends=[ f"{TREEPATH}/leaf_treeshap.hpp", f"{TREEPATH}/progressbar.hpp",
                                      f"{TREEPATH}/recursive_treeshap.hpp", f"{TREEPATH}/utils.hpp",
                                      f"{TREEPATH}/waterfall_treeshap.hpp"])]
